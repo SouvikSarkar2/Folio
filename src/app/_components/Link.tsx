9f246c00aeb3af5544ea5d6b65c341a16b0fc3e7
souvik sarkar <12345souvik12345@gmail.com>
2024-05-26 12:03:38 +0530

    Responsive design added

--- src/app/_components/Link.tsx
+++ src/app/_components/Link.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const LinkComponent = ({ name, link }: { name: string; link: string }) => {
   return (
     <Link href={link} target="_blank">
-      <div className="h-[20px] sm:h-[25px] overflow-hidden">
+      <div className="h-[22px] sm:h-[25px] overflow-hidden">
         <div className="hover:-translate-y-7 transition-all duration-500 delay-100">
           <div>{name}</div>
           <div>{name}</div>
